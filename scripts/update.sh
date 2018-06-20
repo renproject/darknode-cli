7d5bc9e8651630292c460dde7d8c6b2f53c565cb
tok-kkk <yunshi@republicprotocol.com>
2018-06-20 13:03:21 +1000

    update will forcefully use the latest version in master instead just pull

---
+++ scripts/update.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+cd ./go/src/github.com/republicprotocol/republic-go
+sudo git checkout master
+sudo git fetch origin master
+sudo git reset --hard origin/master
+cd cmd/darknode
+go install
+cd
+sudo service darknode restart
