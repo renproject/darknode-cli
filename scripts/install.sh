c065263591690dc552ab07f7de84b5d18b4724d5
tok-kkk <yunshi@republicprotocol.com>
2018-06-25 12:39:39 +1000

    little tweak with the install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/sh
 
 # creating working directory
 mkdir -p $HOME/.darknode
@@ -6,16 +6,25 @@ cd $HOME/.darknode
 wget https://darknode.republicprotocol.com/darknode.zip
 unzip darknode.zip
 
+ostype='$(uname -s)'
+cputype='$(uname -m)'
+
 # Download terraform
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-        TERRAFORM_URL="https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip"
-        wget https://darknode.republicprotocol.com/darknode_linux_amd64
-        mv darknode_linux_amd64 ./bin/darknode
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-        TERRAFORM_URL="https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_darwin_amd64.zip"
-        wget https://darknode.republicprotocol.com/darknode_darwin_amd64
-        mv darknode_darwin_amd64 ./bin/darknode
+if [[ $ostype = 'Linux' -a $cputype = 'x86_64' ]]; then
+    TERRAFORM_URL='https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip'
+    wget https://darknode.republicprotocol.com/darknode_linux_amd64
+    mv darknode_linux_amd64 ./bin/darknode
+elif [[ $ostype = 'Darwin' -a $cputype = 'x86_64' ]]; then
+    TERRAFORM_URL='https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_darwin_amd64.zip'
+    wget https://darknode.republicprotocol.com/darknode_darwin_amd64
+    mv darknode_darwin_amd64 ./bin/darknode
+else
+   echo 'unsupported OS type'
+   cd ..
+   rm -rf .darknode
+   exit 1
 fi
+
 chmod +x bin/darknode
 
 wget $TERRAFORM_URL
@@ -33,7 +42,25 @@ rm darknode.zip
 rm terraform.zip
 
 # make sure the binary is installed in the path
-echo 'export PATH=$PATH:$HOME/.darknode/bin' >> $HOME/.profile
+if ! [ -x '$(command -v darknode)' ]; then
+  if test -n $ZSH_VERSION; then
+    echo 'export PATH=$PATH:$HOME/.darknode/bin' >> ~/.zshrc
+    source ~/.zshrc
+  elif test -n $BASH_VERSION; then
+    echo 'export PATH=$PATH:$HOME/.darknode/bin' >> ~/.bash_profile
+    source ~/.bash_profile
+  elif test -n $KSH_VERSION; then
+    echo 'export PATH=$PATH:$HOME/.darknode/bin' >> ~/.kshrc
+    source ~/.kshrc
+  elif test -n $FCEDIT; then
+    echo 'export PATH=$PATH:$HOME/.darknode/bin' >> ~/.kshrc
+    source ~/.kshrc
+  else
+    echo 'Seems your are using a custom sh.'
+    echo 'Please add /.darknode/bin to the PATH variable'
+  fi
+fi
 
 echo ''
-echo 'Done!'
+echo 'Done! Run the command below to begin.'
+echo 'darknode up --help'
