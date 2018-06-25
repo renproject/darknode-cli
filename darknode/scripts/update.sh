82e6e8f188a89d61aca3a32cc0433c2d92d6a583
tok-kkk <yunshi@republicprotocol.com>
2018-06-25 15:15:31 +1000

    update the update.sh

--- darknode/scripts/update.sh
+++ darknode/scripts/update.sh
@@ -2,9 +2,9 @@
 
 cd ./go/src/github.com/republicprotocol/republic-go
 sudo git stash
-sudo git checkout nightly
-sudo git fetch origin nightly
-sudo git reset --hard origin/nightly
+sudo git checkout master
+sudo git fetch origin master
+sudo git reset --hard origin/master
 cd cmd/darknode
 go install
 cd
