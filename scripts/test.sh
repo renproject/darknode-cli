ddc5b5941f7c2a92a731c45dc0db39f45083f82e
tok-kkk <yunshi@republicprotocol.com>
2018-07-03 13:06:46 +1000

    add script for running local test against different vagrant boxes

---
+++ scripts/test.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+runTest()
+{
+  DIR=$1
+  TYPE=$2
+
+  vagrant up
+  vagrant ssh -- -t '
+  curl https://darknode.republicprotocol.com/install.sh -sSf | sh
+  exit
+  '
+  vagrant ssh -- -t '
+  source .profile
+  darknode list
+  sleep 5s
+  exit
+  '
+echo ""if you see cannot find any node ,
+
+  vagrant destory
+}
+
+cd "$HOME/vagrants" || exit 1
+for dir in */ ; do
+  cd "$dir"
+  for type in */ ; do
+    cd "$type"
+    runTest $dir type
+    cd ..
+  done
+done
+
