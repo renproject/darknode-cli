03fc8d86083ee1c59611251685476801aec99a4b
tok-kkk <yunshi@republicprotocol.com>
2018-07-19 16:27:08 +1000

    fix path issue

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -13,7 +13,7 @@ cp scripts/install.sh build/install.sh
 docker-machine create default
 eval $(docker-machine env default) # Setup the environment for the Docker client
 go get github.com/karalabe/xgo
-xgo --targets=darwin/amd64,linux/amd64 .
+xgo --targets=darwin/amd64,linux/amd64 ./cmd
 mv darknode-cli-darwin-10.6-amd64 build/darknode_darwin_amd64
 mv darknode-cli-linux-amd64 build/darknode_linux_amd64
 docker-machine rm -f default
