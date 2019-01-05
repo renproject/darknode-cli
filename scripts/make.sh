6478f301eab07a8fec597c399e001d63947c6ea0
Susruth  Nadimpalli <susruth@republicprotocol.com>
2019-01-05 17:33:17 +1100

    scripts: updated make script to zip resources

--- scripts/make.sh
+++ scripts/make.sh
@@ -7,5 +7,6 @@ xgo -go 1.10 --targets=linux/amd64 .
 mv cmd-linux-amd64 ../target/darknode-cli/bin/darknode_linux_amd64
 mv cmd-darwin-10.6-amd64 ../target/darknode-cli/bin/darknode_darwin_amd64
 
-cd ../target
-zip resources.zip ./resources/*
+cd ../target/resources
+zip -r resources.zip instance scripts
+mv resources.zip ../resources.zip
