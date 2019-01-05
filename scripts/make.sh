6d52e9080d6d5d600d587507954b0dd8d8d8b5ab
Susruth  Nadimpalli <susruth@republicprotocol.com>
2019-01-05 13:21:37 +1100

    scripts: fixed the make script

--- scripts/make.sh
+++ scripts/make.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+mkdir -p ../target/darknode-cli/bin
 
-cd cmd
+cd ../cmd
 xgo -go 1.10 --targets=darwin/amd64 .
 xgo -go 1.10 --targets=linux/amd64 .
-mv cmd-linux-amd64 ../target/bin/darknode_linux_amd64
-mv cmd-darwin-10.6-amd64 ../target/bin/darknode_darwin_amd64
+mv cmd-linux-amd64 ../target/darknode-cli/bin/darknode_linux_amd64
+mv cmd-darwin-10.6-amd64 ../target/darknode-cli/bin/darknode_darwin_amd64
+
+cd ../target
+zip resources.zip ./resources/*
