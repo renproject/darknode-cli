25fa7a41184bcbeef3024d0a4b01c29d7bdb9d7c
tok-kkk <yunshi@republicprotocol.com>
2019-10-23 16:57:28 +1100

    add terraform config template for digital ocean

---
+++ cmd/provider/do_template.go
@@ -0,0 +1,104 @@
+package provider
+
+type doTerraform struct {
+	Name       string
+	Token      string
+	Region     string
+	Size       string
+	ConfigPath string
+	PubKeyPath string
+	PriKeyPath string
+}
+
+var doTemplate = `
+variable "name" {
+	default = "{{.Name}}"
+}
+
+variable "do_token" {
+	default = "{{.Token}}"
+}
+
+variable "region" {
+	default = "{{.Region}}"
+}
+
+variable "size" {
+	default = "{{.Size}}"
+}
+
+variable "config_path" {
+  default = "{{.ConfigPath}}"
+}
+
+variable "pub_key" {
+  default = "{{.PubKeyPath}}"
+}
+
+variable "pri_key" {
+  default = "{{.PriKeyPath}}"
+}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+resource "digitalocean_ssh_key" "darknode" {
+   name       = var.name
+   public_key = file(var.pub_key)
+}
+
+resource "digitalocean_droplet" "darknode" {
+  provider   = digitalocean
+  image      = "ubuntu-18-04-x64"
+  name       = var.name
+  region     = var.region
+  size       = var.size
+  monitoring = true
+
+  ssh_keys   = [
+    digitalocean_ssh_key.darknode.id
+  ]
+
+  provisioner "remote-exec" {
+	inline = [
+		"curl https://releases.renproject.io/darknode-cli/init.sh -sSf | sh"
+	]
+
+    connection {
+      host        = self.ipv4_address
+      type        = "ssh"
+      user        = "root"
+      private_key = file(var.pri_key)
+    }
+  }
+
+  provisioner "file" {
+    source = var.config_path
+    destination = "$HOME/darknode-config.json"
+
+    connection {
+      host        = self.ipv4_address
+      type        = "ssh"
+      user        = "darknode"
+      private_key = file(var.pri_key)
+    }
+  }
+
+  provisioner "remote-exec" {
+	inline = [
+		"curl https://releases.renproject.io/darknode-cli/install.sh -sSf | sh"
+	]
+
+    connection {
+      host        = self.ipv4_address
+      type        = "ssh"
+      user        = "darknode"
+      private_key = file(var.pri_key)
+    }
+  }
+}
+
+output "multiaddress" {
+  value  = "/ip4/${digitalocean_droplet.darknode.ipv4_address}/tcp/18514/ren/${var.address}"
+}`
