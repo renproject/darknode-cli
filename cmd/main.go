19e9b9cec87920893dd85be28708ca4b46696137
Vince Au <vinceau09@gmail.com>
2019-11-05 14:34:06 +1100

    cmd: split version string when checking for updates

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"os"
 	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/fatih/color"
@@ -183,7 +184,8 @@ func main() {
 
 // checkUpdates fetches the latest release of `darknode-cli` from github and compare the versions. It warns the user if
 // current version is older than the latest release.
-func checkUpdates(curVer string) {
+func checkUpdates(versionString string) {
+	curVer := strings.Split(versionString, "-")[0]
 
 	// Get latest release
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
